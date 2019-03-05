0291811bbeb1b16872bc30c503aaf32cd11112b3
Marc Murray <mmarc.96@gmail.com>
2019-03-05 04:17:05 +0000

    Combine request handler into controls.

--- static/hostels/javascript/MapPainter.js
+++ static/hostels/javascript/MapPainter.js
@@ -1,64 +1,63 @@
-class MapPainter {
-    WRAPPER = $('#map-wrapper');
-    width = WRAPPER.width();
-    height = WRAPPER.height();
-    projection = d3.geoMercator();
-    path = d3.geoPath().projection(projection);
+const wrapper = d3.select('#map-wrapper');
+let width = wrapper.width;
+let height = wrapper.height;
+let projection = d3.geoMercator();
+let path = d3.geoPath().projection(projection);
 
-    constructor() {
-        //Create SVG element and add inside wrapper.
-        this.svg = WRAPPER.append("svg")
-            .attr("height", height)
-            .attr("width", width);
-        //Add zoom functionality and limit panning/zooming.
-        this.svg.call(d3.zoom()
-            .scaleExtent([1, 25])
-            .translateExtent([[0, 0], [width, height]])
-            .on("zoom", zoom));
-    }
+//Create SVG element and add inside wrapper.
+let svg = wrapper.append("svg")
+    .attr("height", height)
+    .attr("width", width);
 
-    drawMap(data) {
-        this.projection.fitExtent([[20, 20], [width - 20, height - 20]], data);
+//Add zoom functionality and limit panning/zooming.
+svg.call(d3.zoom()
+            .scaleExtent([1,25])
+            .translateExtent([[0,0],[width,height]])
+            .on("zoom",zoom));
+d3.json("{% static 'hostels/maps/topo_eer.json' %}").then(drawMap);
 
-        svg.selectAll("path")
-            .data(data.features)
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .attr("class", "region");
 
-        svg.append("path")
-            .datum(topojson.mesh(data, data.objects.eer))
-            .attr("d", path)
-            .attr("class", "border");
-    }
+function drawMap(data) {
+    projection.fitExtent([[20, 20], [width - 20, height - 20]], data);
 
-    drawHostels(data) {
-        svg.selectAll("path")
-            .data(data)
-            .enter()
-            .append("circle")
-            .attr("class", "hostel")
-            .attr("cx", function (d) {
-                return projection(d.geometry.coordinates)[0];
-            })
-            .attr("cy", function (d) {
-                return projection(d.geometry.coordinates)[1];
-            })
-            .attr("r", "2px")
-            .attr("fill", "red")
-            .attr('transform', d3.event.transform)
-            .style('r', 1 / d3.event.transform.k + 'px');
-    }
+    svg.selectAll("path")
+        .data(data.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("class", "region");
 
-    zoom() {
-        svg.selectAll("path")
-            .attr('transform', d3.event.transform)
-            .style('stroke-width', 1 / d3.event.transform.k + 'px');
-        svg.selectAll("circle")
-            .attr('transform', d3.event.transform)
-            .attr('r', 2 / d3.event.transform.k + 'px')
+    svg.append("path")
+        .datum(topojson.mesh(data, data.objects.eer))
+        .attr("d", path)
+        .attr("class", "border");
+}
 
-    }
+function drawHostels(data) {
+    svg.selectAll("path")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("class", "hostel")
+        .attr("cx", function (d) {
+            return projection(d.geometry.coordinates)[0];
+        })
+        .attr("cy", function (d) {
+            return projection(d.geometry.coordinates)[1];
+        })
+        .attr("r", "2px")
+        .attr("fill", "red")
 
 }
+
+function zoom() {
+    svg.selectAll("path")
+        .attr('transform', d3.event.transform)
+        .style('stroke-width', 1 / d3.event.transform.k + 'px');
+    svg.selectAll("circle")
+        .attr('transform', d3.event.transform)
+        .attr('r', 2 / d3.event.transform.k + 'px')
+
+}
+
+
